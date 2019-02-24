ffd78883509a8630edacac1bd3de9052de32dab6
russney <rney@wharton.upenn.edu>
2019-02-24 16:40:47 -0500

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,15 +1,18 @@
 #sudo su -
-cd /usr/local/src
+useradd -u 2000:2000 amos_user
+mkdir /amos
+chown amos_user /amos
+su - amos_user
+cd ~
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh > install.sh
 bash install.sh
 . ~/.nvm/nvm.sh
+cd /amos
 nvm install 11.10.0
 node -e "console.log('Running Node.js ' + process.version)"
 npm i express-generator
 npm install
 export PATH=$PATH:~/node_modules/.bin
-mkdir /amos
-cd /amos
 express
 npm i
 npm start
