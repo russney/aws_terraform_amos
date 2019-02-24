b9a67d713d2828fe41c7c8eca3719dae01ba6811
russney <rney@wharton.upenn.edu>
2019-02-24 17:36:05 -0500

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,7 +1,7 @@
 #sudo su -
 useradd -u 2000 amos_user
-mkdir /amos
-chown amos_user /amos
+mkdir /amos_code
+chown amos_user /amos_code
 cp boot2.sh ~amos_user
 chown amos_user ~amos_user/boot2.sh
 su - amos_user
