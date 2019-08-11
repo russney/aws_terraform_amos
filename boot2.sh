d09fe21e571cb5ab67ccfb317ccf6b2c69c38deb
russney <rney@wharton.upenn.edu>
2019-08-11 14:47:41 -0400

    updated nvm from v0.32.0 to v0.34.0

--- boot2.sh
+++ boot2.sh
@@ -1,5 +1,5 @@
 cd ~
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh > install.sh
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh > install.sh
 bash install.sh
 . ~/.nvm/nvm.sh
 nvm install 11.10.0
