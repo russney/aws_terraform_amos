0513960f6f9a0490d8e367efdb7c9d2a1298fadd
root <root@ip-172-31-41-206.us-east-2.compute.internal>
2019-08-12 01:13:41 +0000

    fixed messup

--- boot2.sh
+++ boot2.sh
@@ -1,15 +1,15 @@
 cd ~
-#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh > install.sh
-#bash install.sh
-#. ~/.nvm/nvm.sh
-#nvm install 11.10.0
-#node -e "console.log('Running Node.js ' + process.version)"
-#npm i -g express-generator
-#cd /amos_code
-##export PATH=$PATH:/amos_code/node_modules/.bin
-#express
-#cd ..; git clone https://github.com/russney/amos_code.git
-#cd /amos_code
-#npm i
-#npm start
-## open port 3000 in AWS security group
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh > install.sh
+bash install.sh
+. ~/.nvm/nvm.sh
+nvm install 11.10.0
+node -e "console.log('Running Node.js ' + process.version)"
+npm i -g express-generator
+cd /amos_code
+#export PATH=$PATH:/amos_code/node_modules/.bin
+express
+cd ..; git clone https://github.com/russney/amos_code.git
+cd /amos_code
+npm i
+npm start
+# open port 3000 in AWS security group
